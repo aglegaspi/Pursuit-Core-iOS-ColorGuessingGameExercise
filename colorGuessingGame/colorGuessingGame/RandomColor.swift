9e2cbe23d205e2f210233dbfc5d9f82cbfe41d92
aglegaspi <lex83nyc@gmail.com>
2019-07-30 23:02:54 -0400

    first commit

---
+++ colorGuessingGame/colorGuessingGame/RandomColor.swift
@@ -0,0 +1,66 @@
+//  RandomColor.swift
+//  colorGuessingGame
+
+//  Created by Alexander George Legaspi on 7/30/19.
+//  Copyright © 2019 Alexander George Legaspi. All rights reserved.
+
+import Foundation
+import UIKit
+
+func randomRed() -> CGFloat {
+    return CGFloat.random(in: 0.0...1.0)
+}
+
+func randomGreen() -> CGFloat {
+    return CGFloat.random(in: 0.0...1)
+}
+
+func randomBlue() -> CGFloat {
+    return CGFloat.random(in: 0.0...1.0)
+}
+
+struct Color {
+    var red: CGFloat
+    var green: CGFloat
+    var blue: CGFloat
+    
+    func changeColor() -> UIColor {
+        return UIColor.init(red: self.red, green: self.green, blue: self.blue, alpha: 1.0)
+    }
+    
+    static func generateColor() -> Color {
+        return Color.init(red: randomRed(), green: randomGreen(), blue: randomBlue())
+    }
+    
+    func compareValues() -> Int {
+        let rgbArray: [CGFloat] = [red,green,blue]
+        
+        for (x,y) in rgbArray.enumerated() {
+            
+            if y == rgbArray.max() {
+                print("At the index: \(x)")
+                print("Highest current value is \(y)")
+                switch x {
+                case 0:
+                    return 1
+                case 1:
+                    return 2
+                case 2:
+                    return 3
+                default:
+                    return 0
+                }
+            }
+            
+        }
+        
+        
+        return 0
+    
+    }
+    
+}
+
+
+
+
---
+++ colorGuessingGame/colorGuessingGame/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  colorGuessingGame
+//
+//  Created by Alexander George Legaspi on 7/30/19.
+//  Copyright © 2019 Alexander George Legaspi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var counter = 0
+    
+    @IBOutlet weak var showMeTheColor: UIImageView!
+    
+    @IBOutlet weak var winOrLose: UILabel!
+    
+    @IBAction func guessColor(_ sender: UIButton) {
+        let selection = Color.generateColor().compareValues()
+        
+        self.showMeTheColor.backgroundColor = Color.generateColor().changeColor()
+        
+        if sender.tag == selection {
+            self.winOrLose.text = "You Won"
+            counter += 1
+        } else {
+            self.winOrLose.text = "You Lost"
+            counter = 0
+        }
+        self.currentScore.text = "Current Score: \(counter)"
+    }
+    
+
+    @IBOutlet weak var currentScore: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.showMeTheColor.backgroundColor = Color.generateColor().changeColor()
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
